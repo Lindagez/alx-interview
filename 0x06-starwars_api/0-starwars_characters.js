e009645fc3f2e7f52fdddbbe5418294626f7d07c
lindagez <lidiyagez@gmail.com>
2023-08-10 10:32:43 -0700

    starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,16 +1,33 @@
-import requests
+#!/usr/bin/node
 
-def get_characters(movie_id):
-    url = f"https://swapi.dev/api/films/{movie_id}/"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        characters = data["characters"]
-        for character in characters:
-            print(character["name"])
-    else:
-        print(f"Error: {response.status_code}")
+const request = require('request');
 
-if __name__ == "__main__":
-    movie_id = int(input("Enter the movie ID: "))
-    get_characters(movie_id)
+const getCharacters = (movieId) => {
+  const url = `https://swapi.dev/api/films/${movieId}/`;
+  request(url, (err, response, body) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      console.error(`Failed to get characters: ${response.statusCode}`);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    const characters = data.characters;
+    for (const character of characters) {
+      console.log(character.name);
+    }
+  });
+};
+
+if (process.argv.length !== 2) {
+  console.error('Usage: ./0-starwars_characters.js <movie_id>');
+  return;
+}
+
+const movieId = process.argv[2];
+getCharacters(movieId);
+
